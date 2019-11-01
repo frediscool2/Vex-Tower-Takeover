40b378df94ab5af0d0b0ff767998032acafa4ecf
Zurtar <ethan.cook3552@gmail.com>
2019-10-31 23:21:39 -0400

    Documented and fixed our image decoding

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -13,36 +13,54 @@ class Animation {
 
 public:
   static void updateScreen() {
-    std::vector<unsigned char> png;
-    std::vector<unsigned char> image;
-    int start = clock() / 10;
+    // waitimes
+    double waitTimes[6] = {250, 250, 250, 250, 250, 250};
+
+    // filepath setup
     std::string filePaths[6] = {"ball-2.png", "ball-3.png", "ball-4.png",
                                 "ball-5.png", "ball-6.png", "ball-7.png"};
-    double waitTimes[6] = {250, 250, 250, 250, 250, 250};
-    int i = 0, itterator = 1;
 
+    // setting up stuff for our loops and start time
+    int i = 0, itterator = 1, start = clock() / 10;
+
+    // setting up our png and image
+    unsigned char *image = 0;
+    unsigned char *png = 0;
+
+    // width and height
     unsigned width, height;
-    static_assert(std::is_same<unsigned char, uint8_t>::value, "foo");
+
+    // pngsize
+    size_t pngsize;
 
     while (true) {
-      load_file(png, filePaths[i].data());
-      decode(image, width, height, png);
+      // Loading and decoding the file to raw 32 bit rgb with lodePNG
+      lodepng_load_file(&png, &pngsize, filePaths[i].data());
+      lodepng_decode32(&image, &width, &height, png, pngsize);
 
-      uint8_t *buffer = image.data();
+      // converting our image data to a buffer
+      uint32_t *buffer = (uint32_t *)image;
 
-      Brain.Screen.drawImageFromBuffer(buffer, 0, 0, image.size());
+      // printing to a screen
+      Brain.Screen.drawImageFromBuffer(buffer, 0, 0, 480, 272);
 
+      // i++
       if (i <= 0) {
         itterator = 1;
       }
 
+      // i--
       if (i >= 5) {
         itterator = -1;
       }
 
+      // reset start
       start = clock() / 10;
+
+      // add itterator
       i += itterator;
 
+      // sleep task
       wait(waitTimes[i], msec);
     }
   }
