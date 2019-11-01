22080e99ec96a7a6c730d1bbee313c4f553b87b0
Zurtar <ethan.cook3552@gmail.com>
2019-11-01 13:09:28 -0400

    fixed some animation

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -8,63 +8,43 @@
 using namespace lodepng;
 
 class Animation {
-public:
-  static int updateScreen() {
-    // waitimes
-    double waitTimes[6] = {250, 250, 250, 250, 250, 250};
+#define YSIZE 272;
+#define XSIZE 480;
 
-    // filepath setup
+public:
+  static void updateScreen() {
+    std::vector<unsigned char> png;
+    std::vector<unsigned char> image;
+    int start = clock() / 10;
     std::string filePaths[6] = {"ball-2.png", "ball-3.png", "ball-4.png",
                                 "ball-5.png", "ball-6.png", "ball-7.png"};
+    double waitTimes[6] = {250, 250, 250, 250, 250, 250};
+    int i = 0, itterator = 1;
 
-    // setting up stuff for our loops and start time
-    int i = 0, itterator = 1, start = clock() / 10;
-
-    // setting up our png and image
-    unsigned char *image = 0;
-    unsigned char *png = 0;
-
-    // width and height
     unsigned width, height;
 
-    // pngsize
-    size_t pngsize;
-
     while (true) {
-      // Loading and decoding the file to raw 32 bit rgb with lodePNG
-      lodepng_load_file(&png, &pngsize, filePaths[i].data());
-      lodepng_decode32(&image, &width, &height, png, pngsize);
+      load_file(png, filePaths[i].data());
+      decode(image, width, height, png);
 
-      // converting our image data to a buffer
-      uint32_t *buffer = (uint32_t *)image;
+      uint8_t *buffer = image.data();
 
-      // printing to a screen
-      Brain.Screen.drawImageFromBuffer(buffer, 0, 0, 480, 272);
+      Brain.Screen.drawImageFromBuffer(buffer, 0, 0, image.size());
 
-      // i++
       if (i <= 0) {
         itterator = 1;
       }
 
-      // i--
       if (i >= 5) {
         itterator = -1;
       }
 
-      // reset start
       start = clock() / 10;
-
-      // add itterator
       i += itterator;
 
-      // sleep task
       wait(waitTimes[i], msec);
     }
-    //will never hit here
-    return -1;
   }
-
-private:
   static void generateRawPixelData(unsigned width, unsigned height) {
     // our buffer
     uint32_t *buffer;
@@ -75,7 +55,6 @@ private:
     // checking if buffer is null
     if (buffer != NULL) {
       printf("buffer is good\n");
-
       // Loop for rows
       for (int y = 0; y < height; y++) {
         // loop for colums
