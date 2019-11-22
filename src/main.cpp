37265ea65fdd0c0f9793784b802dfa0158625df7
Thayen <thayburt@gmail.com>
2019-11-22 16:23:48 -0500

    Added Andrews control scheme

--- src/main.cpp
+++ src/main.cpp
@@ -104,7 +104,44 @@ void SetDriverCharlie()
 
 void SetDriverAndrew()
 {
+  // Robot Movement Setup
+  // Tank Controls
 
+  // Set the left joystick Y Axis to move the left side of the robot
+  Controller.Axis3.changed(ControllerInteraction::moveLeftSide);
+
+  // Set the right joystick Y Axis to move the right side of the robot
+  Controller.Axis2.changed(ControllerInteraction::moveRightSide);
+
+
+  // Arm Movement Setup
+  // Controls for the arm
+
+  // Sets the left down button to lower the robot's arms
+  Controller.ButtonDown.pressed(ControllerInteraction::lowerArm);
+
+  // Sets the right down button(B) to raise the robot's arm
+  Controller.ButtonB.pressed(ControllerInteraction::liftArm);
+
+
+  // Piston Control Setup
+  // Controls for the piston
+
+  // Sets the lowest front left button to lower the piston
+  Controller.ButtonL2.pressed(ControllerInteraction::pistonRetract);
+  
+  // Sets the lowest front right button to raise the piston
+  Controller.ButtonR2.pressed(ControllerInteraction::pistonExtend);
+
+
+  // Intake Control Setup
+  // Controls for the intake system
+
+  // Sets the highest front left button to have the intake system pull objects towards the bot
+  Controller.ButtonL1.pressed(ControllerInteraction::pullIntake);
+
+  // Sets the highest front right button to have the intake system push objects away from the bot
+  Controller.ButtonR1.pressed(ControllerInteraction::pushIntake);
 }
 
 void usercontrol(void) {
