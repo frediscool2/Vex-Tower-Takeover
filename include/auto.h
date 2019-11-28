fb2ab994862d23f8d352a2047dc17ccdac67320d
Zurtar <ethan.cook3552@gmail.com>
2019-11-28 17:02:56 -0500

    pushey ushy

--- include/auto.h
+++ include/auto.h
@@ -6,11 +6,12 @@ const double cmPerDegree = (3.1459 * 8.255) / 360;
 const double mmPerDegree = (3.14159 * 82.55) / 360;
 }; // namespace Math
 
-class Auto {
-  static void rightAuto(int side);
-  static void leftAuto(int side);
-  static void driveForDistance(double distanceVal, motor_group motorGroup,
-                               bool wait = false, double velVal = 50,
-                               velocityUnits velcUnits = velocityUnits::pct,
-                               distanceUnits distanceUnit = inches);
-};
+class Auto;
+void blueLeftAuto();
+static void blueRightAuto();
+static void redLeftAuto();
+static void redRightAuto();
+static void driveForDistance(double distanceVal, motor_group motorGroup,
+                             bool wait = false, double velVal = 50,
+                             velocityUnits velcUnits = velocityUnits::pct,
+                             distanceUnits distanceUnit = inches);
